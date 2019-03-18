cfe94d62ed8f4fdeb68ea1741e1f3f6e5a94b095
Sambhav Gore <sambhavgore@gmail.com>
2019-03-18 15:19:49 +0100

    cleanup

--- dist/js/bs1.js
+++ dist/js/bs1.js
@@ -3,13 +3,13 @@
 
 $(document).ready(function() {
   $("#Quay-NelsonStreet").click(function() {
-    $(".img-nelsonSt").fadeToggle();
+    $("#nelsonStModal").modal("show");
     //animaiton complete
   });
 
   $("#LeonardLane").click(function() {
     console.log($(this));
-    $(".leonardLaneImgs").fadeToggle();
+    $("#leonardLaneModal").modal("show");
     //animaiton complete
   });
 });
@@ -44,50 +44,54 @@ $("#StokesCroft").tooltipster({
 //********************************************************************** */
 //IMAGE SLIDeR
 
-const imageArea = document.getElementById("sidle"),
+const makeSlider = imageArea => {
   images = imageArea.getElementsByClassName("image");
 
-if (images.length > 1) {
-  for (var i = 0; i < images.length; i++) {
-    let topEdge = (100 / images.length) * i + 1,
-      bottomEdge = topEdge + 100 / images.length - 1;
-    images[
-      i
-    ].style.clipPath = `polygon(0% ${topEdge}%, 100% ${topEdge}%, 100% ${bottomEdge}%, 0% ${bottomEdge}%)`;
-  }
-  imageArea.classList.add("slideAnimation");
-
-  function slideAnimation(imgTarget, direction) {
-    let slidy = imgTarget.animate(
-      [
-        { transform: "translateX(0%)", clipPath: imgTarget.style.clipPath },
-        { transform: "translateX(101%)", clipPath: imgTarget.style.clipPath },
-        { transform: "translateX(101%)", clipPath: imgTarget.style.clipPath },
-        {
-          transform: "translateX(101%)",
-          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
-        }
-      ],
-      {
-        duration: 666,
-        easing: "linear",
-        fill: "forwards",
-        direction: direction
-      }
-    );
-    imgTarget.classList.toggle("full");
-  }
-  imageArea.addEventListener("click", function(e) {
-    let fullImage = document.querySelector(".full");
-    if (fullImage == null) {
-      slideAnimation(e.target, "normal");
-    } else {
-      slideAnimation(fullImage, "reverse");
-      if (e.target !== fullImage) {
-        setTimeout(function() {
-          slideAnimation(e.target, "normal");
-        }, 200);
-      }
+  if (images.length > 1) {
+    for (var i = 0; i < images.length; i++) {
+      let topEdge = (100 / images.length) * i + 1,
+        bottomEdge = topEdge + 100 / images.length - 1;
+      images[
+        i
+      ].style.clipPath = `polygon(0% ${topEdge}%, 100% ${topEdge}%, 100% ${bottomEdge}%, 0% ${bottomEdge}%)`;
     }
-  });
-}
+    imageArea.classList.add("slideAnimation");
+
+    function slideAnimation(imgTarget, direction) {
+      let slidy = imgTarget.animate(
+        [
+          { transform: "translateX(0%)", clipPath: imgTarget.style.clipPath },
+          { transform: "translateX(101%)", clipPath: imgTarget.style.clipPath },
+          { transform: "translateX(101%)", clipPath: imgTarget.style.clipPath },
+          {
+            transform: "translateX(101%)",
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
+          }
+        ],
+        {
+          duration: 666,
+          easing: "linear",
+          fill: "forwards",
+          direction: direction
+        }
+      );
+      imgTarget.classList.toggle("full");
+    }
+    imageArea.addEventListener("click", function(e) {
+      let fullImage = document.querySelector(".full");
+      if (fullImage == null) {
+        slideAnimation(e.target, "normal");
+      } else {
+        slideAnimation(fullImage, "reverse");
+        if (e.target !== fullImage) {
+          setTimeout(function() {
+            slideAnimation(e.target, "normal");
+          }, 200);
+        }
+      }
+    });
+  }
+};
+
+makeSlider(document.getElementById("nelsonStImgs"));
+makeSlider(document.getElementById("leonardLaneImgs"));
